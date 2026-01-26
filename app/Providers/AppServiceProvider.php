92c8f7b4ca8ce28b196b58b7e41e2b82b9e46eea
Carl97VM <barcacarlosv@gmail.com>
2026-01-25 22:21:27 -0400

    ci: fix dia3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Log::withContext([
+            'release' => trim(@file_get_contents(base_path('RELEASE_ID'))) ?: 'local',
+            'env' => config('app.env'),
+            'user_id' => Auth::id() ?? 'guest',
+        ]);
     }
 }
